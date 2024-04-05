6d07cf30d26160b86b8191fb938ce2cb7d99f850
Nathan Parker <nathan@Nathans-MacBook-Air.local>
2024-04-05 10:34:12 -0500

    cleaned up data file to import to database

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,12 +1,9 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time with time zone,
+  quaketime timestamp with time zone,
   latitude real,
   longitude real,
   quakedepth real,
   mag real,
-  id text,
   place text,
-  quaketype text
 );
