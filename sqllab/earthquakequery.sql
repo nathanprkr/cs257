b985278b7080a50af48fdb0056417e81e2e5f5f8
nathanprkr <165683530+nathanprkr@users.noreply.github.com>
2024-04-10 10:25:56 -0500

    Update earthquakequery.sql

--- sqllab/earthquakequery.sql
+++ sqllab/earthquakequery.sql
@@ -1,1 +1,10 @@
+-- all earthquakes that have negative longitude
+SELECT * FROM earthquakes WHERE longitude<0 ORDER BY longitude DESC;
 
+-- all earthquakes that have positive longitude and are greater than 4 magnitude
+SELECT * FROM earthquakes WHERE longitude>0
+INTERSECT
+SELECT * FROM earthquakes WHERE mag>6 ORDER BY longitude DESC;
+
+-- all earthquakes deeper than 600 miles
+SELECT * FROM earthquakes WHERE quakedepth>600 ORDER BY quakedepth DESC;
