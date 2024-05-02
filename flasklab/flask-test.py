f2789e07dcce8123956950f5ff43c6b2f548765a
Nathan Parker <nathan@Nathans-MacBook-Air.local>
2024-05-02 14:41:18 -0500

    flasklab

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -18,7 +18,7 @@ def my_color(word1):
 
 @app.route('/add/<num1>/<num2>')
 def add(num1, num2):
-    return ("%i", num1 + num2)
+    return (num1 + num2)
 
 if __name__ == '__main__':
     my_port = 5122
